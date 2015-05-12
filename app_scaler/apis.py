7779ea957d9adc83e20676c949c43d46a447f09f
Ubuntu <ubuntu@ip-172-31-36-169.eu-west-1.compute.internal>
2015-05-12 15:37:38 +0000

    UI for queues

--- app_scaler/apis.py
+++ app_scaler/apis.py
@@ -1,11 +1,9 @@
 import tornado.web
 from tornado.gen import coroutine
-import tornado.ioloop
-import os
 from tornado import template
 import redis
-import pickle
-from app_scaler.facades import AppRepoFacade, AppFacade
+import json
+from app_scaler.facades import AppRepoFacade
 from app_scaler.config import Config
 
 class GUI(tornado.web.RequestHandler):
@@ -20,37 +18,11 @@ class ServerAppApi(tornado.web.RequestHandler):
     def post(self):
         config = Config()
         repo = AppRepoFacade(config)
-        app = AppFacade(config)
         db = redis.StrictRedis()        
         f_info = self.request.files['fileToUpload'][0]
         f_name = f_info['filename']
         java_class = self.get_argument("javaClass")
         backend = self.get_argument("backendName")
-        db.set(backend, pickle.dumps([f_name, java_class]))
+        db.hset(backend, 'app_info', json.dumps({'name': f_name, 'java_class': java_class}))
         repo.put(f_name, f_info['body'])
-        yield app.simple_register(backend)
-
-class RemoteAppApi(tornado.web.RequestHandler):
-
-    def initialize(self, repo_facade):
-        self.app_repo = repo_facade
-
-    def get(self, *args, **kwargs):
-
-        action = self.get_argument("action")
-        name = self.get_argument("name")
-        java_class = self.get_argument("java_class")
-        if action=='start': 
-            obj = self.app_repo.get(name)
-            app_path = os.path.join('/tmp', 'apps', name) 
-            with open(app_path, 'wb') as out:
-                out.write(obj)
-            command = 'sudo rund -cp {} {} &'.format(app_path, java_class)
-            print(command)
-            os.popen(command)
-        elif action=='stop':
-            command = 'killall dalvikvm'
-            print(command)
-            os.popen(command)
-
 
